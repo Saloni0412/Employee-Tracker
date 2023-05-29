af104e5af0b940a27023ef35d220f90a5c8c5fdc
Saloni0412 <pshashikantb@gmail.com>
2023-05-28 23:05:25 -0400

    this commit adds server.js file and fuction to promt questions

---
+++ server.js
@@ -0,0 +1,70 @@
+const express = require("express");
+const mysql = require("mysql2");
+const inquirer = require("inquirer");
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: "localhost",
+    user: "root",
+    password: "root",
+  },
+);
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+const questions = [
+  {
+    type: "list",
+    name: "databaseQuestion",
+    choices: [
+      "view all departments",
+      "view all roles",
+      "view all employees",
+      "add a department",
+      "add a role",
+      "add an employee",
+      "update an employee role",
+    ]
+  }
+]
+
+// function to prompt questions
+let init = function() {
+  inquirer.prompt(questions).then((data) => {
+    console.log(data)
+  
+const { databaseQuestion } = data;
+
+if (databaseQuestion === "view all departments") {
+  viewDepartments();
+} else if (databaseQuestion === "view all roles") {
+  viewRoles();
+} else if (databaseQuestion === "view all employees") {
+  viewEmployees();
+} else if (databaseQuestion === "add a department") {
+  addDepartment();
+} else if (databaseQuestion === "add a role") {
+  addRole();
+} else if (databaseQuestion === "add an employee") {
+  addEmployee()
+} else if (databaseQuestion === "update an employee role") {
+  UpdateRole();
+}
+})
+}
+
+init();
+
+viewDepartments() {
+
+}
